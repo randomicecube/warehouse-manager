0531619f0e6f60b56dfde2631584e565d9340d22
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2021-10-22 00:24:10 +0100

    Add being able to register a partner

--- ggc-app/src/ggc/app/partners/DoRegisterPartner.java
+++ ggc-app/src/ggc/app/partners/DoRegisterPartner.java
@@ -3,7 +3,9 @@ package ggc.app.partners;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 import ggc.WarehouseManager;
-//FIXME import classes
+
+import ggc.app.exceptions.DuplicatePartnerKeyException;
+import ggc.exceptions.PartnerKeyAlreadyUsedException;
 
 /**
  * Register new partner.
@@ -12,12 +14,22 @@ class DoRegisterPartner extends Command<WarehouseManager> {
 
   DoRegisterPartner(WarehouseManager receiver) {
     super(Label.REGISTER_PARTNER, receiver);
-    //FIXME add command fields
+    addStringField("key", Prompt.partnerKey());
+    addStringField("name", Prompt.partnerName());
+    addStringField("address", Prompt.partnerAddress());
   }
 
   @Override
   public void execute() throws CommandException {
-    //FIXME implement command
+    try {
+      _receiver.registerPartner(
+        stringField("key"),
+        stringField("name"),
+        stringField("address")
+      );
+    } catch (PartnerKeyAlreadyUsedException e) {
+      throw new DuplicatePartnerKeyException(e.getKey());
+    }
   }
 
 }
